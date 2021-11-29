e43b5e799a1f606cc79ec70ff42cdad5f1980e29
RobertT122 <robert@tetrault.org>
2021-11-29 09:30:25 -0500

    create table for reviews

--- db/migrate/20211129141903_create_reviews.rb
+++ db/migrate/20211129141903_create_reviews.rb
@@ -1,8 +1,13 @@
 class CreateReviews < ActiveRecord::Migration[5.2]
   def change
     create_table :reviews do |t|
-
+      t.integer :rating, null: false, limit: 1
+      t.string :body, null: false
+      t.integer :user_id, null: false
+      t.integer :game_id, null: false
       t.timestamps
     end
+
+    add_index :reviews, [:user_id, :game_id], unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_29_003051) do
+ActiveRecord::Schema.define(version: 2021_11_29_141903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,16 @@ ActiveRecord::Schema.define(version: 2021_11_29_003051) do
     t.string "artist", null: false
     t.integer "year", limit: 2, null: false
     t.index ["name"], name: "index_games_on_name", unique: true
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating", limit: 2, null: false
+    t.string "body", null: false
+    t.integer "user_id", null: false
+    t.integer "game_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "game_id"], name: "index_reviews_on_user_id_and_game_id", unique: true
   end
 
   create_table "users", force: :cascade do |t|
