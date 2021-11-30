88d22fdfaa2adc3972839dafefea28a623f33777
RobertT122 <robert@tetrault.org>
2021-11-30 10:43:39 -0500

    create review util

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -4,7 +4,7 @@ class Api::ReviewsController < ApplicationController
     @review = Review.new(review_params)
     if @review.save
       update_game_rating!(@review[:game_id])
-      
+      render "api/reviews/show"
     else
       render json: @user.errors.full_messages, status: 422
     end
@@ -14,16 +14,31 @@ class Api::ReviewsController < ApplicationController
   def update
     # update average rating
     @review = Review.find_by(id: params[id])
-
     if @review
-      update_game_rating!(@review[:game_id])
+      if @review.update(review_params)
+        update_game_rating!(@review[:game_id])
+        render "api/reviews/show"
+      else
+        render json: @user.errors.full_messages, status: 422
+      end
     else
-
+      render json: @user.errors.full_messages, status: 422
     end
 
   end
 
   def destroy
     # update average rating
+    @review = Review.find_by(id: params[id])
+    if @review.destroy
+      update_game_rating!(@review[:game_id])
+      render "api/reviews/show"
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :user_id, :game_id, :body)
   end
 end
--- frontend/util/reviews_util.js
+++ frontend/util/reviews_util.js
@@ -4,3 +4,24 @@ export const fetchReviews = game_id => (
     url: `api/games/${game_id}/reviews`
   })
 )
+
+export const createReview = review => (
+  $.ajax({
+    method: "POST",
+    url: "api/reviews",
+    data: review
+  })
+)
+export const updateReview = review => (
+  $.ajax({
+    method: "PATCH",
+    url: `api/reviews/${review.id}`,
+    data: review
+  })
+)
+export const createReview = review_id => (
+  $.ajax({
+    method: "DELETE",
+    url: `api/reviews/${review_id}`,
+  })
+)
