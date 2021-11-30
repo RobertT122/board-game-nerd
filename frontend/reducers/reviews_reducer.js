551d2b7bff8fdb84f4ae47cda0db9c579a95de26
RobertT122 <robert@tetrault.org>
2021-11-30 11:13:28 -0500

    add reviews cases to reviews reducer

--- frontend/reducers/reviews_reducer.js
+++ frontend/reducers/reviews_reducer.js
@@ -1,10 +1,16 @@
-import { RECIVE_GAME_REVIEWS } from "../actions/review_actions";
+import * as ReviewActions from "../actions/review_actions";
 
 const reviewsReducer = (state = {}, action ) => {
   Object.freeze(state);
   switch(action.type){
-    case RECIVE_GAME_REVIEWS:
+    case ReviewActions.RECIVE_GAME_REVIEWS:
       return Object.assign({}, action.payload.reviews)
+    case ReviewActions.RECIEVE_REVIEW:
+      return Object.assign({}, state, {[actions.review.id]: action.review})
+    case ReviewActions.DELETE_REVIEW:
+      let newState = Object.assign({}, state)
+      delete newState[action.review.id];
+      return newState;
     default:
       return state
   }
