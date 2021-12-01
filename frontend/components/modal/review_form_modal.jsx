417c5f0c80617252ebde0056053fe520b08732c5
RobertT122 <robert@tetrault.org>
2021-12-01 09:57:29 -0500

    add delete functionality to button

--- frontend/components/modal/review_form_modal.jsx
+++ frontend/components/modal/review_form_modal.jsx
@@ -1,21 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect} from "react-redux";
 import { useLocation, matchPath } from "react-router";
 import { createReview, deleteReview, updateReview } from "../../actions/review_actions";
 
 
 const ReviewFormModal = props => {
+
+  //setup the any prefiled data for the user id
   let location = useLocation()
   let match = matchPath(location.pathname, {
     path: "/game/:game_id/"
   })
-  const game_id = match.params.game_id
-  console.log(game_id)
+  let game_id = match.params.game_id
+  let currentUserReview = props.findUserReviewId(props.currUserId)
+  let action = props.createReview
+  let deleteReview = () => null
+  if (currentUserReview){
+    action = props.updateReview
+    deleteReview = () => props.deleteReview(currentUserReview.id)
+  } else {
+    currentUserReview = {body: "", rating: 0, game_id, user_id: props.currUserId}
+  }
 
-  const currentUserReview = props.findUserReviewId(props.currUserId)
-  console.log(currentUserReview)
+  let [state, setState] = useState(currentUserReview)
+  
+  const handleSubmit = e => {
+    e.preventDefault()
+    action(state)
+  }
+
+  const handleChange = input => (
+    e => setState(Object.assign({},state,{[input]: e.currentTarget.value}))
+  )
+
   return (
-    <h1>reviewForm</h1>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="number"
+          value={state.rating} 
+          onChange={handleChange("rating")}
+        />
+        <input 
+          type="text"
+          value={state.body}
+          onChange={handleChange("body")}
+        />
+        <button type="submit">submit</button>
+      </form>
+      <button onClick={deleteReview}>Delete</button>
+    </div>
   )
 }
 
@@ -27,18 +61,17 @@ const mapSTP = state => ({
 
   findUserReviewId: (userId) => {
     let reviews = state.entities.reviews;
-    return Object.keys(reviews).find(key =>(
+    let key = Object.keys(reviews).find(key =>(
       reviews[key].userId === userId
     ))
+    return reviews[key]
   },
-
-
 })
 
 const mapDTP = dispatch => ({
-  deleteReview: () => dispatch(deleteReview),
-  createReview: () => dispatch(createReview),
-  updateReview: () => dispatch(updateReview),
+  deleteReview: review_id => dispatch(deleteReview(review_id)),
+  createReview: review => dispatch(createReview(review)),
+  updateReview: review => dispatch(updateReview(review)),
 })
 
 const ReviewFormModalContainer = connect(mapSTP, mapDTP)(ReviewFormModal)
