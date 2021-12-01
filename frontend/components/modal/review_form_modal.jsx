6a486f2cb38dcf335522d05d89d39e5a441904d1
RobertT122 <robert@tetrault.org>
2021-12-01 17:09:18 -0500

    finalize review for Heroku deployment

--- frontend/components/modal/review_form_modal.jsx
+++ frontend/components/modal/review_form_modal.jsx
@@ -31,9 +31,19 @@ const ReviewFormModal = props => {
   }
 
   const handleChange = input => (
-    e => setState(Object.assign({},state,{[input]: e.currentTarget.value}))
+    e => {
+      setState(Object.assign({},state,{[input]: e.currentTarget.value}))
+    }
   )
 
+  const isChecked = value => {
+    if(state.rating === value){
+      return true
+    } else
+    {
+      return false
+    }
+  }
 
   return (
     <>
@@ -42,54 +52,63 @@ const ReviewFormModal = props => {
 
         <div className="form-input rating-stars">
           <label><span>1</span>
-            <input type="radio" className="star" value="1" name="rating" 
-              onClick={handleChange("rating")} 
+            <input type="radio" className="star" value="1" name="rating" defaultChecked={isChecked(1)}
+              onClick={handleChange("rating")}
               // onMouseEnter={ e => e.currentTarget.classList.add("checked")} 
               // onMouseLeave={ e => e.currentTarget.classList.remove("checked")}
             />
           </label>
           <label><span>2</span>
             <input type="radio" className="star" value="2" name="rating" 
+              defaultChecked={isChecked(2)}
               onClick={handleChange("rating")} 
             />
           </label>
           <label><span>3</span>
             <input type="radio" className="star" value="3" name="rating" 
+            defaultChecked={isChecked(3)}
               onClick={handleChange("rating")} 
             />
           </label>
           <label><span>4</span>
             <input type="radio" className="star" value="4" name="rating" 
+              defaultChecked={isChecked(4)}
               onClick={handleChange("rating")} 
             />
           </label>
           <label><span>5</span>
             <input type="radio" className="star" value="5" name="rating" 
+              defaultChecked={isChecked(5)}
               onClick={handleChange("rating")} 
             />
           </label>
           <label><span>6</span>
             <input type="radio" className="star" value="6" name="rating" 
+              defaultChecked={isChecked(6)}
               onClick={handleChange("rating")} 
             />
           </label>
           <label><span>7</span>
             <input type="radio" className="star" value="7" name="rating" 
+              defaultChecked={isChecked(7)}
               onClick={handleChange("rating")} 
             />
           </label>
           <label><span>8</span>
             <input type="radio" className="star" value="8" name="rating" 
+              defaultChecked={isChecked(8)}
               onClick={handleChange("rating")} 
             />
           </label>
           <label><span>9</span>
             <input type="radio" className="star" value="9" name="rating" 
+              defaultChecked={isChecked(9)}
               onClick={handleChange("rating")} 
             />
           </label>
           <label><span>10</span>
             <input type="radio" className="star" value="10" name="rating" 
+              defaultChecked={isChecked(10)}
               onClick={handleChange("rating")} 
             />
           </label>
