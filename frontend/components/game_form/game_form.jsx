0da07945bbbb20ce0e2e63e750e470d23072be53
RobertT122 <robert@tetrault.org>
2022-01-23 16:50:33 -0500

    Setup form for game creation

--- frontend/components/game_form/game_form.jsx
+++ frontend/components/game_form/game_form.jsx
@@ -3,10 +3,35 @@ import { useState } from "react";
 
 
 const Game_Form = (props) =>{
+  let [state, setState] = useState({
+
+  });
+
+  let [range, setRange] = useState({timeLimit: false, playerCount: false});
+
+  const handleInput = e => {
+
+  }
+
+  const handleSubmit = e => {
+
+  }
 
   return(
     <form>
+      <label htmlFor=""> </label>
+      <input 
+        id=""
+        type="text" 
+      />
 
+      <label htmlFor="game-image">Choose game image:</label>
+      <input 
+        type="file"
+        accept="image/png, image/jpeg"
+        id="game-image"
+
+      />
     </form>
   )
 }
