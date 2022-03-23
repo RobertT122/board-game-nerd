6a9c126be09f963cabe8fb73d86ad218ee21cc5e
RobertT122 <robert@tetrault.org>
2022-03-22 23:51:15 -0400

    Add percentage and refactor reviewAnalytics to reduce code

--- frontend/components/reviews/reviewAnalytics.jsx
+++ frontend/components/reviews/reviewAnalytics.jsx
@@ -4,60 +4,36 @@ const ReviewAnalitics = ({reviews}) => {
   let ratingCount = {[1]:0, [2]:0, [3]:0, [4]:0, [5]:0, [6]:0, [7]:0, [8]:0, [9]:0, [10]:0}
 
   reviews.forEach(review => ratingCount[review.rating] += 1)
+
+  let ratingJSX = {}
+  Object.keys(ratingCount).forEach(key => {
+    if (ratingCount[key]) {
+      ratingJSX[key] = <li key={key}>
+        <span className="numberKey">{key}</span>
+
+        <div style={{backgroundColor: 'blue', height: 10, width: (ratingCount[key]/reviews.length)*100 }}></div>
+        <span>{(ratingCount[key]/reviews.length).toFixed(3) * 100} %</span>
+      </li>
+    } else {
+      ratingJSX[key] = <></>
+    }
+  })
   return(
     <div>
       <ol className="review-Counts">
-        <li>
-          <span>1:</span>
-          <div style={{backgroundColor: 'red', height: 10, width: (ratingCount[1]/reviews.length)*400 }}></div>
-        </li>
-
-        <li>
-          <span>2:</span>
-          <div style={{backgroundColor: 'red', height: 10, width: (ratingCount[2]/reviews.length)*400 }}></div>
-        </li>
-
-        <li>
-          <span>3:</span>
-          <div style={{backgroundColor: 'red', height: 10, width: (ratingCount[3]/reviews.length)*400 }}></div>
-        </li>
-
-        <li>
-          <span>4:</span>
-          <div style={{backgroundColor: 'red', height: 10, width: (ratingCount[4]/reviews.length)*400 }}></div>
-        </li>
-
-        <li>
-          <span>5:</span>
-          <div style={{backgroundColor: 'gold', height: 10, width: (ratingCount[5]/reviews.length)*400 }}></div>
-        </li>
-
-        <li>
-          <span>6:</span>
-          <div style={{backgroundColor: 'gold', height: 10, width: (ratingCount[6]/reviews.length)*400 }}></div>
-        </li>
-
-        <li>
-          <span>7:</span>
-          <div style={{backgroundColor: 'gold', height: 10, width: (ratingCount[7]/reviews.length)*400 }}></div>
-        </li>
-
-        <li>
-          <span>8:</span>
-          <div style={{backgroundColor: 'green', height: 10, width: (ratingCount[8]/reviews.length)*400 }}></div>
-        </li>
-
-        <li>
-          <span>9:</span>
-          <div style={{backgroundColor: 'green', height: 10, width: (ratingCount[9]/reviews.length)*400 }}></div>
-        </li>
-
-        <li>
-          <span>10:</span>
-          <div style={{backgroundColor: 'green', height: 10, width: (ratingCount[10]/reviews.length)*400 }}></div>
-        </li>
+        {ratingJSX[1]}
+        {ratingJSX[2]}
+        {ratingJSX[3]}
+        {ratingJSX[4]}
+        {ratingJSX[5]}
+        {ratingJSX[6]}
+        {ratingJSX[7]}
+        {ratingJSX[8]}
+        {ratingJSX[9]}
+        {ratingJSX[10]}
       </ol>
     </div>
+
   )
 
 }
